33aeb04787d22fcd72ccbceb48f5e6438304a881
Kristof Szabo <Kristof.Szabo@logmein.com>
2022-10-07 23:29:46 +0200

    unnecessary code removed

--- MongoDbOptions.cs
+++ MongoDbOptions.cs
@@ -4,5 +4,4 @@ public class MongoDbOptions
 {
     public string ConnectionString { get; set; } = null!;
     public string Database { get; set; } = null!;
-    public string BeerCollectionName { get; set; } = null!;
 }
